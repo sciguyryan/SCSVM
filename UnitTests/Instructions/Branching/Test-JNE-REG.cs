3aed87f253f14c46360f31657f7bc888a7d2d693
Ryan Jones-Ward <sciguyryan@gmail.com>
2020-05-17 16:55:44 +0100

    More JNE_REG tests
    A test for checking that attempting to bing a label to an argument that does not support it throws an exception at compile time.
    
    A test for checking that binding to a non-existent label throws an exception  at compile time.

--- UnitTests/Instructions/Branching/Test-JNE-REG.cs
+++ UnitTests/Instructions/Branching/Test-JNE-REG.cs
@@ -1,4 +1,6 @@
-﻿using VMCore;
+﻿using System;
+using System.IO;
+using VMCore;
 using VMCore.Assembler;
 using VMCore.VM;
 using VMCore.VM.Core.Exceptions;
@@ -15,7 +17,8 @@ namespace UnitTests.Instructions
         }
 
         /// <summary>
-        /// Test JNE with valid destination, jump condition met.
+        /// Test to ensure that jump does occur when
+        /// jump condition is met.
         /// </summary>
         [TestMethod]
         public void TestUserAssemblyJumpTakenValid()
@@ -39,12 +42,17 @@ namespace UnitTests.Instructions
 
             var program = new QuickIns[]
             {
-                new QuickIns(OpCode.MOV_LIT_REG, new object[] { 100, r1 }),
-                new QuickIns(OpCode.SUB_LIT_REG, new object[] { 50, r1 }),
-                new QuickIns(OpCode.MOV_LIT_REG, new object[] { 100, r2 }),
-                new QuickIns(OpCode.JNE_REG, new object[] { r2, destOffset }),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r1 }),
+                new QuickIns(OpCode.SUB_LIT_REG,
+                             new object[] { 50, r1 }),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r2 }),
+                new QuickIns(OpCode.JNE_REG,
+                             new object[] { r2, destOffset }),
                 new QuickIns(OpCode.HLT),
-                new QuickIns(OpCode.MOV_LIT_REG, new object[] { expected, r3 }),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { expected, r3 }),
             };
 
             _vm.Run(Utils.QuickRawCompile(program));
@@ -53,7 +61,8 @@ namespace UnitTests.Instructions
         }
 
         /// <summary>
-        /// Test JNE with valid destination, jump condition not met.
+        /// Test to ensure that jump does not occur when
+        /// jump condition is not met.
         /// </summary>
         [TestMethod]
         public void TestUserAssemblyJumpNotTakenValid()
@@ -78,13 +87,19 @@ namespace UnitTests.Instructions
 
             var program = new QuickIns[]
             {
-                new QuickIns(OpCode.MOV_LIT_REG, new object[] { 100, r1 }),
-                new QuickIns(OpCode.SUB_LIT_REG, new object[] { 50, r1 }),  // AC = 50
-                new QuickIns(OpCode.MOV_LIT_REG, new object[] { 50, r2 }),
-                new QuickIns(OpCode.JNE_REG, new object[] { r2, destOffset }),
-                new QuickIns(OpCode.MOV_LIT_REG, new object[] { expected, r3 }),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r1 }),
+                new QuickIns(OpCode.SUB_LIT_REG,
+                             new object[] { 50, r1 }),  // AC = 50
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 50, r2 }),
+                new QuickIns(OpCode.JNE_REG,
+                             new object[] { r2, destOffset }),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { expected, r3 }),
                 new QuickIns(OpCode.HLT),
-                new QuickIns(OpCode.MOV_LIT_REG, new object[] { fail, r3 }),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { fail, r3 }),
             };
 
             _vm.Run(Utils.QuickRawCompile(program));
@@ -94,7 +109,8 @@ namespace UnitTests.Instructions
 
 
         /// <summary>
-        /// Test JNE with valid label destination.
+        /// Test to ensure that jumping to a valid label
+        /// destination works as expected.
         /// </summary>
         [TestMethod]
         public void TestUserAssemblyValidLabel()
@@ -119,8 +135,7 @@ namespace UnitTests.Instructions
                 new QuickIns(OpCode.MOV_LIT_REG,
                              new object[] { fail, r3 }),
                 new QuickIns(OpCode.HLT),
-                new QuickIns(OpCode.LABEL,
-                             new object[] { "GOOD" }),
+                new QuickIns(OpCode.LABEL, new object[] { "GOOD" }),
                 new QuickIns(OpCode.MOV_LIT_REG,
                              new object[] { expected, r3 }),
             };
@@ -128,6 +143,55 @@ namespace UnitTests.Instructions
             _vm.Run(Utils.QuickRawCompile(program));
 
             Assert.IsTrue(_vm.CPU.Registers[r3] == expected);
+        }
+
+        /// <summary>
+        /// Test JNE with invalid label. As the label
+        /// name is missing then an exception should 
+        /// be thrown at compile time.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidDataException))]
+        public void TestUserAssemblyInvalidLabel()
+        {
+            var r1 = Registers.R1;
+            var r2 = Registers.R2;
+
+            var program = new QuickIns[]
+            {
+                new QuickIns(OpCode.SUB_LIT_REG,
+                             new object[] { 50, r1 }),  // AC = -50
+                new QuickIns(OpCode.JNE_REG,
+                             new object[] { r2, 0 },
+                             new AsmLabel("A", 1)),
+            };
+
+            _vm.Run(Utils.QuickRawCompile(program));
+        }
+
+        /// <summary>
+        /// Test binding a label to invalid argument.
+        /// This should throw an exception as argument
+        /// 0 cannot support label binding.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestUserAssemblyInvalidArgumentBind()
+        {
+            var r1 = Registers.R1;
+            var r2 = Registers.R2;
+
+            var program = new QuickIns[]
+            {
+                new QuickIns(OpCode.SUB_LIT_REG,
+                             new object[] { 50, r1 }),  // AC = -50
+                new QuickIns(OpCode.JNE_REG,
+                             new object[] { r2, 0 },
+                             new AsmLabel("A", 0)),
+                new QuickIns(OpCode.LABEL, new object[] { "A" }),
+            };
+
+            _vm.Run(Utils.QuickRawCompile(program));
         }
 
         /// <summary>
@@ -145,10 +209,14 @@ namespace UnitTests.Instructions
 
             var program = new QuickIns[]
             {
-                new QuickIns(OpCode.MOV_LIT_REG, new object[] { 100, r1 }),
-                new QuickIns(OpCode.SUB_LIT_REG, new object[] { 50, r1 }),  // AC = 50
-                new QuickIns(OpCode.MOV_LIT_REG, new object[] { 100, r2 }),
-                new QuickIns(OpCode.JNE_REG, new object[] { r2, -2 }),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r1 }),
+                new QuickIns(OpCode.SUB_LIT_REG,
+                             new object[] { 50, r1 }),  // AC = 50
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r2 }),
+                new QuickIns(OpCode.JNE_REG,
+                             new object[] { r2, -2 }),
             };
 
             _vm.Run(Utils.QuickRawCompile(program));

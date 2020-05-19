ebf7b57f54d4467b8df1b08e2ce163571d5a2e40
Ryan Jones-Ward <sciguyryan@gmail.com>
2020-05-19 14:44:38 +0100

    Implement JGE_REG instruction and associated tests. Closes #11. All tests pass

---
+++ UnitTests/Instructions/Branching/Test-JGE_REG.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.IO;
+using VMCore;
+using VMCore.Assembler;
+using VMCore.VM;
+using VMCore.VM.Core.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Instructions
+{
+    [TestClass]
+    public class Test_JGE_REG
+        : Test_Instruction_Base
+    {
+        public Test_JGE_REG()
+        {
+        }
+
+        /// <summary>
+        /// Test to ensure that jump does occur when
+        /// jump condition is met.
+        /// </summary>
+        [TestMethod]
+        public void TestUserAssemblyJumpTakenValid()
+        {
+            var r1 = Registers.R1;
+            var r2 = Registers.R2;
+            var r3 = Registers.R3;
+            const int expected = 0x123;
+
+            // This is calculated as follows.
+            // sizeof(OpCode) * 5 for the number of
+            // instructions to skip.
+            // sizeof(int) * 4 for the number of integer
+            // arguments to be skipped.
+            // sizeof(Registers) * 4 for the number of
+            // register arguments to be skipped.
+            const int destOffset =
+                sizeof(OpCode) * 5 +
+                sizeof(int) * 4 +
+                sizeof(Registers) * 4;
+
+            var program = new QuickIns[]
+            {
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r1 }),
+                new QuickIns(OpCode.SUB_LIT_REG,
+                             new object[] { 50, r1 }),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r2 }),
+                new QuickIns(OpCode.JGE_REG,
+                             new object[] { r2, destOffset }),
+                new QuickIns(OpCode.HLT),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { expected, r3 }),
+            };
+
+            _vm.Run(Utils.QuickRawCompile(program));
+
+            Assert.IsTrue(_vm.CPU.Registers[r3] == expected);
+        }
+
+        /// <summary>
+        /// Test to ensure that jump does not occur when
+        /// jump condition is not met.
+        /// </summary>
+        [TestMethod]
+        public void TestUserAssemblyJumpNotTakenValid()
+        {
+            var r1 = Registers.R1;
+            var r2 = Registers.R2;
+            var r3 = Registers.R3;
+            const int expected = 0x123;
+            const int fail = 0x321;
+
+            // This is calculated as follows.
+            // sizeof(OpCode) * 6 for the number of
+            // instructions to skip.
+            // sizeof(int) * 5 for the number of integer
+            // arguments to be skipped.
+            // sizeof(Registers) * 5 for the number of
+            // register arguments to be skipped.
+            const int destOffset =
+                sizeof(OpCode) * 6 +
+                sizeof(int) * 5 +
+                sizeof(Registers) * 5;
+
+            var program = new QuickIns[]
+            {
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r1 }),
+                new QuickIns(OpCode.SUB_LIT_REG,
+                             new object[] { 50, r1 }),  // AC = 50
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 40, r2 }),
+                new QuickIns(OpCode.JGE_REG,
+                             new object[] { r2, destOffset }),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { expected, r3 }),
+                new QuickIns(OpCode.HLT),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { fail, r3 }),
+            };
+
+            _vm.Run(Utils.QuickRawCompile(program));
+
+            Assert.IsTrue(_vm.CPU.Registers[r3] == expected);
+        }
+
+
+        /// <summary>
+        /// Test to ensure that jumping to a valid label
+        /// destination works as expected.
+        /// </summary>
+        [TestMethod]
+        public void TestUserAssemblyValidLabel()
+        {
+            var r1 = Registers.R1;
+            var r2 = Registers.R2;
+            var r3 = Registers.R3;
+            const int expected = 0x123;
+            const int fail = 0x321;
+
+            var program = new QuickIns[]
+            {
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r1 }),
+                new QuickIns(OpCode.SUB_LIT_REG,
+                             new object[] { 50, r1 }),  // AC = 50
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r2 }),
+                new QuickIns(OpCode.JGE_REG,
+                             new object[] { r2, 0 },
+                             new AsmLabel("GOOD", 1)),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { fail, r3 }),
+                new QuickIns(OpCode.HLT),
+                new QuickIns(OpCode.LABEL, new object[] { "GOOD" }),
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { expected, r3 }),
+            };
+
+            _vm.Run(Utils.QuickRawCompile(program));
+
+            Assert.IsTrue(_vm.CPU.Registers[r3] == expected);
+        }
+
+        /// <summary>
+        /// Test jump with invalid label. As the label
+        /// name is missing then an exception should 
+        /// be thrown at compile time.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidDataException))]
+        public void TestUserAssemblyInvalidLabel()
+        {
+            var r1 = Registers.R1;
+            var r2 = Registers.R2;
+
+            var program = new QuickIns[]
+            {
+                new QuickIns(OpCode.SUB_LIT_REG,
+                             new object[] { 50, r1 }),  // AC = -50
+                new QuickIns(OpCode.JGE_REG,
+                             new object[] { r2, 0 },
+                             new AsmLabel("A", 1)),
+            };
+
+            _vm.Run(Utils.QuickRawCompile(program));
+        }
+
+        /// <summary>
+        /// Test binding a label to invalid argument.
+        /// This should throw an exception as argument
+        /// 0 cannot support label binding.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestUserAssemblyInvalidArgumentBind()
+        {
+            var r1 = Registers.R1;
+            var r2 = Registers.R2;
+
+            var program = new QuickIns[]
+            {
+                new QuickIns(OpCode.SUB_LIT_REG,
+                             new object[] { 50, r1 }),  // AC = -50
+                new QuickIns(OpCode.JGE_REG,
+                             new object[] { r2, 0 },
+                             new AsmLabel("A", 0)),
+                new QuickIns(OpCode.LABEL, new object[] { "A" }),
+            };
+
+            _vm.Run(Utils.QuickRawCompile(program));
+        }
+
+        /// <summary>
+        /// Test jump with invalid destination. This will crash
+        /// as soon as an attempt is made to fetch and execute
+        /// the next instruction as the instruction will be read
+        /// from non-executable memory.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(MemoryAccessViolationException))]
+        public void TestUserAssemblyJumpInvalid()
+        {
+            var r1 = Registers.R1;
+            var r2 = Registers.R2;
+
+            var program = new QuickIns[]
+            {
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r1 }),
+                new QuickIns(OpCode.SUB_LIT_REG,
+                             new object[] { 50, r1 }),  // AC = 50
+                new QuickIns(OpCode.MOV_LIT_REG,
+                             new object[] { 100, r2 }),
+                new QuickIns(OpCode.JGE_REG,
+                             new object[] { r2, -2 }),
+            };
+
+            _vm.Run(Utils.QuickRawCompile(program));
+
+            _vm.CPU.FetchExecuteNextInstruction();
+        }
+    }
+}
---
+++ VM/VM/Instructions/Branching/JGE_REG.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using VMCore.VM.Core;
+
+namespace VMCore.VM.Instructions
+{
+    internal class JGE_REG
+        : Instruction
+    {
+        public override Type[] ArgumentTypes =>
+            new Type[]
+            {
+                typeof(Registers),
+                typeof(int)
+            };
+
+        public override Type[] ExpressionArgumentTypes =>
+            new Type[]
+            {
+                null,
+                null
+            };
+
+        public override OpCode OpCode =>
+            OpCode.JGE_REG;
+
+        public override string AsmName => "jge";
+
+        public override bool CanBindToLabel(int aArgumentID)
+        {
+            return (aArgumentID == 1);
+        }
+
+        public override bool Execute(InstructionData aData, CPU aCpu)
+        {
+            if (aCpu.Registers[(Registers)aData[0]] >=
+                aCpu.Registers[Registers.AC])
+            {
+                // Offset the address by current base size of the memory.
+                // This is the bound of the memory outside of the 
+                // executable memory region (e.g. main memory and stack).
+                aCpu.Registers[Registers.IP] =
+                    aCpu.VM.Memory.BaseMemorySize + (int)aData[1];
+            }
+
+            return false;
+        }
+
+        public override string ToString(InstructionData aData)
+        {
+            var fromReg = (Registers)aData[0];
+            var address = (int)aData[1];
+
+            // jge R1, $ADDRESS
+            return $"{AsmName} {fromReg}, ${address:X}";
+        }
+    }
+}

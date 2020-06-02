f8cbc58463c0e79b7b74920cbb9ae0de04f3449c
Ryan Jones-Ward <sciguyryan@gmail.com>
2020-06-02 10:29:04 +0100

    Add SUBROTUNES.cs file
    Actually add the SUBROTUNES.cs file this time.

---
+++ VM/VM/Instructions/Special/SUBROUTINE.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using VMCore.VM.Core;
+
+namespace VMCore.VM.Instructions.Special
+{
+    internal class SUBROUTINE
+        : Instruction
+    {
+        public override Type[] ArgumentTypes =>
+            new Type[]
+            {
+                typeof(int)
+            };
+
+        public override Type[] ExpressionArgumentTypes =>
+            new Type[]
+            {
+                null
+            };
+
+        public override InsArgTypes[] ArgumentRefTypes =>
+            new InsArgTypes[]
+            {
+                InsArgTypes.LiteralInteger
+            };
+
+        public override OpCode OpCode =>
+            OpCode.SUBROUTINE;
+
+        public override string AsmName => "";
+
+        public override bool Execute(InstructionData aData, Cpu aCpu)
+        {
+            return false;
+        }
+
+        public override string ToString(InstructionData aData)
+        {
+            // @SUB[ID]:
+            return $"@SUB{(int)aData[0]}:";
+        }
+    }
+}
